8f7e01bc38685cfa0cef8b9b9e521bd9c38905d1
hernan plaza <hplaza@lya-electronic.com>
2022-07-08 11:17:01 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Techno Apes</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVJm_MRCPhnwft6X9MmTVINOCXQv4YHsI",
+  authDomain: "techno-comics-app.firebaseapp.com",
+  projectId: "techno-comics-app",
+  storageBucket: "techno-comics-app.appspot.com",
+  messagingSenderId: "130633294524",
+  appId: "1:130633294524:web:9e5ee3bfab9c163196e6b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
